f8dc05cc9d41986f6062b0e2f070f80d02fe15e4
Corey Tilly <dezigngamez@gmail.com>
2014-08-10 23:40:38 -0400

    Camera Changes
    Changed camera to follow along with the player instead of being set to
    fixed position.  Player sprite now has a slight jitter when moving.

--- Client/Assets/Scripts/CameraFollow.cs
+++ Client/Assets/Scripts/CameraFollow.cs
@@ -3,48 +3,54 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour 
 {
-	public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
-	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
-	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
-	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
-	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
-	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
-
-
+	//public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
+	//public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
+	//public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	//public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
+	//public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
+	//public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
+	
+	
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
 	private Transform player;		// Reference to the player's transform.
-
-
+	
+	
 	void Awake ()
 	{
 		// Setting up the reference.
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 	}
-
-
-	bool CheckXMargin()
+	
+	
+	/*bool CheckXMargin()
 	{
 		// Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
 		return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
-	}
-
-
-	bool CheckYMargin()
+	}*/
+	
+	
+	/*bool CheckYMargin()
 	{
 		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
 		return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
-	}
-
-
-	void FixedUpdate ()
+	}*/
+	
+	/*void FixedUpdate ()
+	{
+		TrackPlayer();
+	}*/
+	
+	// Changed where the camera postion is updated in an attempt to fix the player sprite jitter
+	void LateUpdate ()
 	{
 		TrackPlayer();
 	}
 	
-	
 	void TrackPlayer ()
 	{
 		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
-		float targetX = transform.position.x;
+		/*float targetX = transform.position.x;
 		float targetY = transform.position.y;
 
 		// If the player has moved beyond the x margin...
@@ -59,9 +65,15 @@ public class CameraFollow : MonoBehaviour
 
 		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
 		targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
-		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
-
-		// Set the camera's position to the target position with the same z component.
-		transform.position = new Vector3(targetX, targetY, transform.position.z);
+		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);*/
+		
+		/*// Set the camera's position to the target position with the same z component.
+		transform.position = new Vector3(targetX, targetY, transform.position.z);*/
+			
+			Vector3 point = camera.WorldToViewportPoint(player.position);
+		Vector3 delta = player.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+		Vector3 destination = transform.position + delta;
+		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		
 	}
 }
