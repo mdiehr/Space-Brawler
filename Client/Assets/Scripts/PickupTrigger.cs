057005cdddf16e3843d172cdc23a9a3c6502335b
Corey Tilly <dezigngamez@gmail.com>
2014-08-17 11:47:28 -0400

    Added comments to script

--- Client/Assets/Scripts/PickupTrigger.cs
+++ Client/Assets/Scripts/PickupTrigger.cs
@@ -6,8 +6,10 @@ public class PickupTrigger : MonoBehaviour {
 	public string playerTag = "Player";
 	public string energyTag = "EnergyOrb";
 
+	// Event that triggers when the player contacts a collider marker as a trigger
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		// Only destroy object if it is tagged as energy orb
 		if ( other.gameObject.tag == energyTag )
 		{
 			//Debug.Log("Energy!!!!");
