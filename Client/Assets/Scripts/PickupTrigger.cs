3c6b94cb060643ac6914ab945e5e671ff273cbd5
Corey Tilly <dezigngamez@gmail.com>
2014-08-17 11:41:05 -0400

    Created generic "energy" orb
    Added sprite and script for an energy pick up, also created a prefab to
    add quickly to the level.

---
+++ Client/Assets/Scripts/PickupTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupTrigger : MonoBehaviour {
+
+	public string playerTag = "Player";
+	public string energyTag = "EnergyOrb";
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if ( other.gameObject.tag == energyTag )
+		{
+			//Debug.Log("Energy!!!!");
+			Destroy (other.gameObject);
+		}
+		//Debug.Log ("Hit" + other.gameObject.tag );
+	}
+}
